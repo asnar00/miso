1c7e31f19e585e53f2283e4798d6f368dc4e09ad
asnar00 <ash.nehru@gmail.com>
2025-08-21 17:49:10 +0100

    after last experiment

---
+++ project/miso/platforms/template/template/templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateApp.swift
+//  template
+//
+//  Created by Ashraf Nehru on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
